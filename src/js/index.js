a71a18239934a5b424cd101a9f07062301fe78a6
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-02-14 10:54:53 -0500

    Performance.now with fallback to Date

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,4 @@
-// Good browsers support window.performance but shitsters like IE and Safari do not
-window.performance = window.performance || {
-  'offset': Date.now(),
-  'now':    function now() {
-    return Date.now() - this.offset
-  }
-}
+import now from './now'
 
-// EG
-alert(window.performance.now())
+alert(now())
+
