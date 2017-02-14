3eefe9c07ed32d728f5b0acc4ca183a2be7ebe1e
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-02-14 14:08:52 -0500

    Circle vanishing over time

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,51 @@
 import now from 'present'
 
+const canvas = document.getElementById('canvas')
+const context = canvas.getContext('2d')
+const centerX = canvas.width / 2
+const centerY = canvas.height / 2
+
+var timeStart = 0
+var timeElapsed = timeStart
+var timeEnd = 180
+var tock = false
+var start = -0.5 * Math.PI
+var end = 1.5 * Math.PI
+var pos = start
+var radius = 200
+var lineWidth = 15
+
+const degToRadians = deg => deg * Math.PI / 180
+const timeToDegrees = t => t * 6
+
+const update = () => {
+  timeElapsed += 1
+}
+
+const drawTime = (minutes) => {
+  let pos = degToRadians(timeToDegrees(minutes))
+  context.beginPath()
+  context.lineWidth = lineWidth
+  context.arc(centerX, centerY, radius, start + pos, end)
+  context.strokeStyle = tock ? 'rgba(0, 0, 255, 0.25)' : 'rgba(255, 0, 0, 0.25)'
+  context.stroke()
+}
+
+const draw = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  let circuits = Math.floor((timeEnd - timeElapsed) / 60)
+  console.log(circuits)
+  for (let i = 0; i < circuits; i++) {
+    drawTime(0)
+  }
+  drawTime(timeElapsed)
+}
+
+
 const loop = (() => {
   var time, timeDelta,
     timeBefore = 0,
-    frameInterval = 10
+    frameInterval = 1000
 
   return () => {
     window.requestAnimationFrame(loop)
@@ -13,7 +55,9 @@ const loop = (() => {
 
     if (timeDelta >= frameInterval) {
       timeBefore = time
-      console.log(timeDelta)
+      console.log(time)
+      update()
+      draw()
     }
   }
 
