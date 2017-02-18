511303a4ae1880ac234d0e95d2167368ef1fcd71
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-02-18 10:35:33 -0500

    Updated babel settings and added lodash

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,20 +5,29 @@ import merge from 'webpack-merge'
 import modules from './conf/webpack'
 
 const DefinePlugin = new webpack.DefinePlugin({
-  'PRODUCTION': JSON.stringify(false),
-  'VERSION':    JSON.stringify('0.1'),
-  'DEBUG':      JSON.stringify(true)
+  PRODUCTION: JSON.stringify(false),
+  VERSION:    JSON.stringify('0.1'),
+  DEBUG:      JSON.stringify(true)
 })
 
 const baseConfig = {
-  'entry':  resolve('src', 'js'),
-  'output': {
-    'path':     resolve('dist'),
-    'filename': 'bundle.js'
+  entry:  resolve('src', 'js'),
+  output: {
+    path:     resolve('dist'),
+    filename: 'bundle.js'
   },
-  'plugins': [
+  plugins: [
     DefinePlugin
-  ]
+  ],
+  module: {
+    loaders: [
+      {
+        test:    /\.jsx?$/,
+        include: resolve('src', 'js'),
+        loader:  'babel-loader'
+      }
+    ]
+  }
 }
 
 export default merge(baseConfig, modules)
