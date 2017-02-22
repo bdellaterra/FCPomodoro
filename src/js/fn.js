4efc60d5dc618352d089c4fc4a77ac675e98128e
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-02-21 19:58:24 -0500

    Adjusted import/export to avoid linter error

--- src/js/fn.js
+++ src/js/fn.js
@@ -1,4 +1,5 @@
-export { pick } from 'lodash/pick'
+import lodashPick from 'lodash/pick'
+export const pick = lodashPick
 export const frozen = Object.freeze
 export const sealed = Object.seal
 export const assign = Object.assign
