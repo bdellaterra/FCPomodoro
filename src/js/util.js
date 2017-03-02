7bad9fffcb10047fa91814f9a40bc28400dd408e
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-02 13:29:11 -0500

    Added generator utility function filterNext()

--- src/js/util.js
+++ src/js/util.js
@@ -1,3 +1,12 @@
 
+// Minimal null-form of an iterator.
 export const nullIterator = (function* () {}())
+
+// Iterate each generator in a list, removing those that are done.
+export const filterNext = (gs) => {
+  return gs.filter((gen) => {
+    let n = gen.next()
+    return !n.done
+  })
+}
 
