330e6b9ea7ccd4583bb625a05903ca8371c95aab
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-03 14:33:32 -0500

    Removed debugging.

--- src/js/util.js
+++ src/js/util.js
@@ -4,11 +4,5 @@ export const nullIterator = (function* () {}())
 
 // Iterate each generator in a list, removing those that are done.
 // If an optional value is provided it is passed to the generators.
-export const filterNext = (gs, v) => {
-  return gs.filter((gen) => {
-    let { done } = gen.next(v)
-    DEBUG && !done && console.log(value)
-    return !done
-  })
-}
+export const filterNext = (gs, v) => gs.filter((g) => !g.next(v).done)
 
