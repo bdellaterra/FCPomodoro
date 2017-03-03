f901e42e5046af43dc28b268d76c06b39173300d
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-02 21:35:34 -0500

    Destructuring assignment for cleaner iteration.

--- src/js/util.js
+++ src/js/util.js
@@ -4,10 +4,10 @@ export const nullIterator = (function* () {}())
 
 // Iterate each generator in a list, removing those that are done.
 // If an optional value is provided it is passed to the generators.
-export const filterNext = (gs, val) => {
+export const filterNext = (gs, v) => {
   return gs.filter((gen) => {
-    let n = gen.next(val)
-    return !n.done
+    let { done } = gen.next(v)
+    return !done
   })
 }
 
