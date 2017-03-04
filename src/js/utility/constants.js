8b73bd743e942b6fbbb9dfd72a67a5abcd16a45b
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-04 00:10:10 -0500

    Adding constants to avoid magic numbers for time values.

---
+++ src/js/utility/constants.js
@@ -0,0 +1,7 @@
+
+// Basic time units converted to miliseconds.
+export const MILISECOND = 1
+export const SECOND = MILISECOND * 1000
+export const MINUTE = SECOND * 60
+export const HOUR = MINUTE * 60
+
