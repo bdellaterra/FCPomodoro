a13f78878293bf45d9378826c9d5c248d9dc590c
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-13 17:27:33 -0400

    Test for whether object is iterable.

--- src/js/utility/iter.js
+++ src/js/utility/iter.js
@@ -2,6 +2,9 @@
 // Minimal null-form of an iterator.
 export const nullIterator = (function* () {}())
 
+// Minimal null-form of an iterator.
+export const isIterable = (x) => x && typeof x[Symbol.iterator] === 'function'
+
 // Iterate each generator in a list, removing those that are done.
 // If an optional value is provided it is passed to the generators.
 export const filterNext = (gs, v) => gs.filter((g) => !g.next(v).done)
