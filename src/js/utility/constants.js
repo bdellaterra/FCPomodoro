5e33cdd2e9d92018f4dbfe68fc1eb1aed2f84473
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-17 11:07:30 -0400

    Add more time constants.

--- src/js/utility/constants.js
+++ src/js/utility/constants.js
@@ -1,5 +1,6 @@
 
 // Time units from a base of one millisecond.
+// ============================================================================
 
 export const MILLISECONDS_PER_SECOND = 1000
 export const SECONDS_PER_MINUTE = 60
@@ -14,7 +15,22 @@ export const SECOND = MILLISECOND * MILLISECONDS_PER_SECOND
 export const MINUTE = SECOND * SECONDS_PER_MINUTE
 export const HOUR = MINUTE * MINUTES_PER_HOUR
 
+// Quick enough to barely be noticable.
+export const BLINK = SECOND / 5
+
+// Safe delay for testing asynchronous timing operations
+export const JIFFY = 50
+
+
+// Margin of error to account for time-shift during operations
+export const MICROJIFFY = JIFFY / 10
+
+// For testing close-to-exact time values
+export const NANOJIFFY = JIFFY / 100
+
+
 // Arc units based in radians
+// ============================================================================
 
 export const ARC_ORIGIN = 0
 export const ARC_CYCLE = 2 * Math.PI
