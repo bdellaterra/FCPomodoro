aada0d0a7cb5bccb18e7c16ce6110bc5ee389dfe
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-20 11:11:34 -0400

    Preserve experiments.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,57 @@
 import 'css/styles.css'
-import { init } from 'app'
 
-init()
+import { makeCountdownTimer } from 'time/countdownTimer'
+import { sleep } from 'time/sleep'
+
+let t = makeCountdownTimer()
+
+t.countdown(3000)
+
+console.log(t.remaining())
+
+t.waitAlarm()
+  .then(() => console.log('Wake up!'))
+  .catch(() => console.log('...zzzzzzzzzzzz...'))
+
+sleep(3000).then(t.sync)
+
+// import { init } from 'app'
+
+// function* notifyer(cb) {
+//   cb()
+// }
+//
+// let state = {}
+//
+// state.promise = new Promise((resolve) => {
+//   state.notifyer = (function* () { resolve() }())
+// })
+//
+// state.promise.then((res) => console.log('Promise done!', res))
+//
+// function* timer() {
+//   let currentTime = -1
+//   while (currentTime = yield) {
+//     console.log('current time:', currentTime)
+//   }
+//   state.notifyer.next(7)
+//   console.log('timer done.')
+// }
+//
+// let t = timer()
+//
+// let now = 5
+// const sync = () => {
+//   t.next(now--)
+// }
+//
+// sync()
+// sync()
+// sync()
+// sync()
+// sync()
+// sync()
+//
+// let w = state.promise
+// w.then(() => console.log('You woke me up too!'))
 
