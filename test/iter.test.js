fb20ebd7cda6e34b231a134c843a2c48409c7b99
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-22 11:00:46 -0400

    Remove unused imports.

--- test/iter.test.js
+++ test/iter.test.js
@@ -1,10 +1,8 @@
 var test = require(process.env.JS_TEST_LIB).test
 
-import { JIFFY, MICROJIFFY, NANOJIFFY } from '../src/js/utility/constants'
 import { filterNext, isIterable, nullIterator, once
        } from '../src/js/utility/iter'
-import { sleep } from '../src/js/time/sleep'
-import now from 'present'
+
 
 test('Null iterator is iterable.', (t) => {
   t.true( isIterable(nullIterator) )
@@ -22,7 +20,7 @@ test('Once() allows a single iteration.', (t) => {
 
 test('filerNext() removes iterators that are done.', (t) => {
   let i = once(() => 0),
-      arr = [nullIterator, i]
+      arr = [nullIterator]
   t.true( arr.length === 2 )
   t.true( filterNext(arr).length === 1 )
   t.true( filterNext(arr).length === 0 )
