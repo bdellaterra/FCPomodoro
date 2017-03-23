168f57555d828d4e42bc8f4927952cfc5497cf7c
Brian Dellaterra <bdellaterra@voodooglobe.com>
2017-03-23 09:24:45 -0400

    Test sleep().

---
+++ test/sleep.test.js
@@ -0,0 +1,14 @@
+var test = require(process.env.JS_TEST_LIB).test
+
+import { JIFFY, NANOJIFFY } from 'utility/constants'
+import { sleep } from 'time/sleep'
+import now from 'present'
+
+
+test('Returns a promise that resolves a specified time later.', async (t) => {
+  const firstTime = now()
+  await sleep(JIFFY)
+  const shift = now() - firstTime - JIFFY
+  t.true(shift < NANOJIFFY)
+})
+
